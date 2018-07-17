a50c3e768025ba42354788fccbb58389d1bf675e
Patricia Coelho Silva <patriciacoelho.slv@gmail.com>
2018-07-17 13:31:08 -0300

    adicina primeira tela a main, permite execução direto pelo eclipse

--- src/com/deadlock/aiehbarril/Main.java
+++ src/com/deadlock/aiehbarril/Main.java
@@ -5,22 +5,73 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
 
 public class Main extends Application {
+
+	private Stage primaryStage;
+    private BorderPane rootLayout;
+
 	@Override
 	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+		this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("");
+
+        initRootLayout();
+
+        showPersonOverview();
+
 	}
 
-	public static void main(String[] args) {
-		launch(args);
-	}
+	/**
+     * Inicializa o root layout (layout base).
+     */
+    public void initRootLayout() {
+        try {
+            // Carrega o root layout do arquivo fxml.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("view/RootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
+
+            // Mostra a scene (cena) contendo o root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Mostra o person overview dentro do root layout.
+     */
+    public void showPersonOverview() {
+        try {
+            // Carrega o person overview.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("view/SearchCourse.fxml"));
+            AnchorPane personOverview = (AnchorPane) loader.load();
+
+            // Define o person overview dentro do root layout.
+            rootLayout.setCenter(personOverview);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Retorna o palco principal.
+     * @return
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
 }
