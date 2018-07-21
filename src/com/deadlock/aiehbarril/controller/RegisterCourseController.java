1db107678efea9e2956062b8f63a0c356b13814b
Patricia Coelho Silva <patriciacoelho.slv@gmail.com>
2018-07-21 13:24:43 -0300

    integra controller a página de cadastrar usuário, add funções ao model e cadastra algumas disciplinas pela Main, integrando model a main

--- src/com/deadlock/aiehbarril/controller/RegisterCourseController.java
+++ src/com/deadlock/aiehbarril/controller/RegisterCourseController.java
@@ -1,5 +1,54 @@
 package com.deadlock.aiehbarril.controller;
 
-public class RegisterCourseController {
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import com.deadlock.aiehbarril.Main;
+import com.deadlock.aiehbarril.model.Course;
 
+public class RegisterCourseController { /*
+
+	@FXML
+    private TableView<Course> personTable;
+    @FXML
+    private TableColumn<Course, String> firstNameColumn;
+    @FXML
+    private TableColumn<Course, String> lastNameColumn;
+
+    @FXML
+    private Label firstNameLabel;
+    @FXML
+    private Label lastNameLabel;
+    @FXML
+    private Label streetLabel;
+    @FXML
+    private Label postalCodeLabel;
+    @FXML
+    private Label cityLabel;
+    @FXML
+    private Label birthdayLabel;
+
+ // Reference to the main application.
+    private Main mainApp;
+
+    public RegisterCourseController() {
+    	//construtor roda antes do initialize()
+    }
+
+    @FXML
+    private void initialize() {/** Eh chamado ao ser carregado o FXML
+        // Inicializa a tablea de pessoa com duas colunas.
+//        firstNameColumn.setCellValueFactory(cellData -> cellData.getValue().firstNameProperty());
+//        lastNameColumn.setCellValueFactory(cellData -> cellData.getValue().lastNameProperty());
+    }
+
+    /** É chamado pela aplicação principal para dar uma referência de volta a si mesmo.
+      *	@param mainApp
+    public void setMainApp(Main mainApp) {
+        this.mainApp = mainApp;
+
+        // Adiciona os dados da observable list na tabela
+//        personTable.setItems(mainApp.getPersonData());
+    }*/
 }
